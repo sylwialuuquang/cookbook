498c41de286cde77b331112b9eb394f10f119124
Sylwia Luu Quang <sylwialuuquang@gmail.com>
2021-04-24 14:02:26 +0200

    models implementation

---
+++ cookbook_backend/cookbook/models.py
@@ -0,0 +1,67 @@
+from django.db.models import Model, CharField, TextField, IntegerField, TextChoices, ForeignKey, CASCADE
+
+
+class Recipe(Model):
+
+    class Category(TextChoices):
+        BREAKFAST = 'Breakfast'
+        LUNCH = 'Lunch'
+        DINNER = 'Dinner'
+        DESSERT = 'Dessert'
+        SUPPER = 'Supper'
+        OTHER = 'Other'
+
+    class Cuisine(TextChoices):
+        AMERICAN = 'American'
+        POLISH = 'Polish'
+        ITALIAN = 'Italian'
+        JAPANESE = 'Japanese'
+        CHINESE = 'Chinese'
+        OTHER = 'Other'
+
+    title = CharField(max_length=128)
+    description = CharField(max_length=256)
+    category = CharField(
+        max_length=9,
+        choices=Category.choices,
+        default=Category.OTHER
+    )
+    cuisine = CharField(
+        max_length=8,
+        choices=Cuisine.choices,
+        default=Cuisine.OTHER
+    )
+    cooktime = IntegerField()
+    serving = CharField(max_length=24)
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(Model):
+
+    class Unit(TextChoices):
+        KG = 'kg'
+        G = 'g'
+        L = 'l'
+        ML = 'ml'
+
+    product = CharField(max_length=24)
+    quantity = IntegerField()
+    unit = CharField(
+        max_length=2,
+        choices=Unit.choices,
+        default=Unit.KG
+    )
+    recipe = ForeignKey(Recipe, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.product + ' ' + str(self.quantity) + self.unit
+
+
+class Instruction(Model):
+    text = TextField()
+    recipe = ForeignKey(Recipe, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.text[:128]
--- cookbook_backend/cookbook_backend/urls.py
+++ cookbook_backend/cookbook_backend/urls.py
@@ -1,20 +1,11 @@
-"""cookbook_backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+
+from cookbook.models import Recipe, Ingredient, Instruction
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Instruction)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
