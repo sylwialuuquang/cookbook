bd3871a3bd35e70a31692bad4a27ac5b6e1ea430
Sylwia Luu Quang <sylwialuuquang@gmail.com>
2021-04-25 13:29:16 +0200

    JWT authentication

--- cookbook_backend/cookbook/views.py
+++ cookbook_backend/cookbook/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Recipe, Ingredient
 from .serializers import RecipeListSerializer, RecipeDetailSerializer, IngredientSerializer
@@ -10,15 +11,18 @@ class RecipeListView(ListAPIView):
     serializer_class = RecipeListSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['title', 'description', 'category', 'cuisine', 'cooktime', 'serving']
+    permission_classes = [IsAuthenticated]
 
 
 class RecipeDetailView(RetrieveAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeDetailSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class IngredientsListView(ListAPIView):
     queryset = Ingredient.objects.values('product').distinct()
     serializer_class = IngredientSerializer
+    permission_classes = [IsAuthenticated]
 
 
--- cookbook_backend/cookbook_backend/urls.py
+++ cookbook_backend/cookbook_backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from cookbook.models import Recipe, Ingredient, Instruction
 
@@ -9,5 +10,7 @@ admin.site.register(Instruction)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('cookbook.urls'))
 ]
